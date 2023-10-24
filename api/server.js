4f2ac3b09e63e0455bc387773372a7e377cdab21
arif.ghani <arif.ghani@shopee.com>
2023-10-24 15:22:15 +0700

    add express

--- api/server.js
+++ api/server.js
@@ -1,42 +1,29 @@
+const express = require('express');
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
 const cors = require('cors');
 
-const server = jsonServer.create();
+const app = express();
+const port = 3000;
 
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
 
-server.db = router.db;
+// Enable CORS with specific options
+const corsOptions = {
+    origin: '*', // Replace '*' with the specific origin(s) you want to allow
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 
-server.use((req, res, next) => {
-    // Set the allowed origins here, or use a dynamic function to check the origin
-    res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with the specific origin(s) you want to allow
+app.use(express.json());
 
-    // Set the allowed methods and headers as needed
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+app.use(auth);
 
-    // Handle preflight requests
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(204);
-    } else {
-        next();
-    }
+// Define API routes
+app.use('/api', router);
+
+app.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
 });
-
-server.use(auth);
-server.use(middlewares);
-
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}));
-
-server.use(router);
-
-server.listen(3000, () => {
-    console.log('JSON Server is running');
-});
-
-module.exports = server;
