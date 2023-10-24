a2a217422f37e3a7256336e1785ed2912865c109
arif.ghani <arif.ghani@shopee.com>
2023-10-24 15:11:41 +0700

    edit cors

--- api/server.js
+++ api/server.js
@@ -1,29 +1,26 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
-const cors = require('cors'); // Add this line to require the cors package
+const cors = require('cors');
 
 const server = jsonServer.create();
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
-
-// Comment out to allow write operations
 const router = jsonServer.router('db.json');
-
 const middlewares = jsonServer.defaults();
 
 server.db = router.db;
 
-server.use(cors()); // Add this line to enable CORS
+// Configure CORS options
+const corsOptions = {
+    origin: '*', // Replace '*' with the specific origin(s) you want to allow or set it to a function that dynamically validates the origin.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization', // Adjust this according to your needs
+    optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+server.use(cors(corsOptions)); // Apply CORS with the defined options
 server.use(auth);
 server.use(middlewares);
 
-// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
@@ -35,5 +32,4 @@ server.listen(3000, () => {
     console.log('JSON Server is running');
 });
 
-// Export the Server API
 module.exports = server;
